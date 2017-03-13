bd949552b7520c77f7f1badb508a0b2b68584a53
KostaNau <naumov.k@gmail.com>
2017-03-13 23:24:09 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+## Description
+The script finds files with equal name and size (without hash comparison).
+
+
+## How to use
+Run the script with additional argument ```-target``` with absolute path to the target directory.
+
+
+## Example
+```python duplicates.py -target /some/path```
+### Output
+```
+File name: filename
+Path: /some/path/dir/filename
+Path: /some/path/other_dir/filename
+==========================================================
+```
+
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,50 @@
+import os
+import itertools
+import argparse
+from collections import defaultdict
+
+
+def parse_arg() -> str:
+    parser = argparse.ArgumentParser(description='Anti-Duplicator. \
+    The script finds files with equal name and size in a target directory.')
+    parser.add_argument('-target', help="path to the target directory")
+    args = parser.parse_args()
+    target_directory = args.target
+    return target_directory
+
+
+def get_filepaths(target_directory: str) -> str:
+    for subdir, dirs, files in os.walk(root_directory):
+        for file in files:
+            file_path = os.path.join(subdir, file)
+            if os.path.isfile(file_path):
+                yield file_path
+
+
+def are_duplicates(filepath1: str, filepath2: str) -> bool:
+    return os.path.basename(filepath1) == os.path.basename(filepath2) and \
+            os.path.getsize(filepath1) == os.path.getsize(filepath2)
+
+
+def find_duplicates(paths_pool: iter) -> defaultdict:
+    duplicates = defaultdict(list)
+    for file_path_1, file_path_2 in itertools.combinations(paths_pool, 2):
+        if are_duplicates(file_path_1, file_path_2):
+            duplicates[os.path.basename(file_path_1)].append(file_path_1)
+            duplicates[os.path.basename(file_path_1)].append(file_path_2)
+    return duplicates
+
+
+def output_duplicates(duplicates: defaultdict) -> None:
+    for name, paths in duplicates.items():
+        print('File name: {}'.format(name))
+        for path in paths:
+            print('Path: {}'.format(path))
+        print('==========================================================')
+
 
 if __name__ == '__main__':
-    pass
+    root_directory = parse_arg()
+    all_paths = get_filepaths(root_directory)
+    duplicates = find_duplicates(all_paths)
+    output_duplicates(duplicates)
